a3251c4159df792ee7e3303d901865d006cf68df
dncrwlye <dancrowley.e@gmail.com>
2019-02-25 11:29:25 -0700

    i have no idea if im doing this right

---
+++ R/data_documentation.R
@@ -0,0 +1,19 @@
+#' Bozeman Bike Commuter Data
+#'
+#' Data from a Bozeman Bike Commuter Counts
+#'
+#' @docType data
+#'
+#' @usage data(data)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(data)
+"data"
+
+LazyData: true
